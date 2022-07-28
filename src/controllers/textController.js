b3453c7a7380e99f29bbe274a6ae9e46bf00d337
Arkangel Contreras <92954009+arkangel3d@users.noreply.github.com>
2022-07-28 18:07:22 -0500

    Update textController.js
    se validan Oraciones palíndromas

--- src/controllers/textController.js
+++ src/controllers/textController.js
@@ -10,6 +10,19 @@ const textReverse = (req, res) => {
   if (!reg.test(text)|| text.length<2) {
     return res.status(400).json({ error: 'invalid text' })
   }
+  if (text.includes(' ') && text.length<3) {
+    const textReverse = text.split('').reverse().join('').replace(/\s+/g, '')
+     if(textReverse.toLowerCase()===text.toLowerCase()){
+       palindrome = true
+       return res.json({
+    text: text,
+    palindrome
+     }
+    return res.json({
+    text: textReverse,
+    palindrome
+  })
+  }
   res.json({
     text: textReverse,
     palindrome
