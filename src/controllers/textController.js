1f1b66a0c795bc2da27ecc40797a95482668fe8c
Arkangel Contreras <92954009+arkangel3d@users.noreply.github.com>
2022-07-28 18:14:13 -0500

    Update textController.js

--- src/controllers/textController.js
+++ src/controllers/textController.js
@@ -1,32 +1,23 @@
-
 const textReverse = (req, res) => {
-  const text = req.query.text
-  const textReverse = text.split('').reverse().join('')
-  const palindrome = text.toLowerCase() === textReverse.toLowerCase()
-  const reg = /^[a-zA-Z\s]*$/
-  if (!text) {
-    return res.status(400).json({ error: 'no text' })
+    const text = req.query.text
+    const textReverse = text.split('').reverse().join('')
+    const palindrome = text.toLowerCase() === textReverse.toLowerCase()
+    const reg = /^[a-zA-Z\s]*$/
+    if (!text) {
+      return res.status(400).json({ error: 'no text' })
+    }
+    if (!reg.test(text)|| text.length<2) {
+      return res.status(400).json({ error: 'invalid text' })
+    }
+    if (text.includes(' ') && text.length>3) {
+      const textReverse = text.split('').reverse().join('').replace(/\s+/g, '')
+       if(textReverse.toLowerCase()===text.toLowerCase()){
+         palindrome = true
+         return res.json({text: text,palindrome})
+         }
+      return res.json({text: textReverse,palindrome})
+    }
+    res.json({text: textReverse,palindrome})
   }
-  if (!reg.test(text)|| text.length<2) {
-    return res.status(400).json({ error: 'invalid text' })
-  }
-  if (text.includes(' ') && text.length<3) {
-    const textReverse = text.split('').reverse().join('').replace(/\s+/g, '')
-     if(textReverse.toLowerCase()===text.toLowerCase()){
-       palindrome = true
-       return res.json({
-    text: text,
-    palindrome
-     }
-    return res.json({
-    text: textReverse,
-    palindrome
-  })
-  }
-  res.json({
-    text: textReverse,
-    palindrome
-  })
-}
 
 export default textReverse
