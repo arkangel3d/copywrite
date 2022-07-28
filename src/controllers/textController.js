6831d3d7b685236b27d5e9f5ed8530e79972b70b
Arkangel Contreras <92954009+arkangel3d@users.noreply.github.com>
2022-07-28 16:11:21 -0500

    Update textController.js
    validación por expresión regular

--- src/controllers/textController.js
+++ src/controllers/textController.js
@@ -3,11 +3,11 @@ const textReverse = (req, res) => {
   const text = req.query.text
   const textReverse = text.split('').reverse().join('')
   const palindrome = text === textReverse
-  const reg = '[A-Za-z]'
+  const reg = /^[A-Z]+$/i
   if (!text) {
     return res.status(400).json({ error: 'no text' })
   }
-  if (!reg.test(text)) {
+  if (!reg.test(text)|| text.length<2) {
     return res.status(400).json({ error: 'invalid text' })
   }
   res.json({
