8559fb079852b53209c9586743bb0d1440838260
arkangel3d <arkangelcontreras@icloud.com>
2022-07-28 00:24:13 -0500

    config cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import router from './routes/index'
+import cors from 'cors'
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use('/', router)
