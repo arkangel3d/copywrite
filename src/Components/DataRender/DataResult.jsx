0a31e66bff446169057c56741347ffaf4667c46b
arkangel3d <arkangelcontreras@icloud.com>
2022-07-28 10:35:42 -0500

    se corrige texto palindromo

--- src/Components/DataRender/DataResult.jsx
+++ src/Components/DataRender/DataResult.jsx
@@ -5,10 +5,10 @@ const DataResult = ({textResult})=>{
   return (
     <>
     <Container className='resultContainer'>
-        <p className='dataResult'> {textResult.text || "Result 1"}</p>
+        <p className='dataResult'> {textResult.text}</p>
     </Container>
     <Container className='resultContainer'>
-        <p className={textResult.palindrome? 'palindrome':'notPalindrome'}> {textResult.palindrome ? "Es una Palindromo" : "No es Palindromo"}</p>
+        <p className={textResult.palindrome? 'palindrome':'notPalindrome'}> {textResult.palindrome ? "It's a palindrome" : "It's not a palindrome"}</p>
     </Container>
     </>
     
