4755b8dc93e503ae22dc67c5b00e6b240fa006f7
Arkangel Contreras <92954009+arkangel3d@users.noreply.github.com>
2022-07-28 15:43:36 -0500

    Update textController.js
    validación de texto

--- src/controllers/textController.js
+++ src/controllers/textController.js
@@ -3,9 +3,13 @@ const textReverse = (req, res) => {
   const text = req.query.text
   const textReverse = text.split('').reverse().join('')
   const palindrome = text === textReverse
+  const reg = '[A-Za-z]'
   if (!text) {
     return res.status(400).json({ error: 'no text' })
-  };
+  }
+  if (!reg.test(text)) {
+    return res.status(400).json({ error: 'invalid text' })
+  }
   res.json({
     text: textReverse,
     palindrome
