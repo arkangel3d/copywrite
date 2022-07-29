11d0dd96511ef9c27309846c22959fe08a4a3d58
arkangel3d <arkangelcontreras@icloud.com>
2022-07-28 20:22:42 -0500

    port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-const port = 3001
+const port = process.env.PORT || 3001
 app.use(express.json())
 app.use('/', router)
 
